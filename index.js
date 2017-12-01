c788113c438bbd3a5a63404d8e8d08f035da18f9
yomoda07 <kanji.yy@gmail.com>
2017-11-30 20:32:05 -0500

    Set up firebase

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
+import * as firebase from 'firebase';
 
-
+var config = {
+    apiKey: "AIzaSyBrmEDw2473a4aG53WSura1XJQpOh-eKoE",
+    authDomain: "snap-up.firebaseapp.com",
+    databaseURL: "https://snap-up.firebaseio.com",
+    projectId: "snap-up",
+    storageBucket: "",
+    messagingSenderId: "754203017870"
+  };
+  firebase.initializeApp(config);
 
 AppRegistry.registerComponent('mobile', () => App);
