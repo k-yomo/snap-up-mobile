347a2797741dc6acd5a4097742c2276260af458f
yomoda07 <kanji.yy@gmail.com>
2017-12-06 18:35:55 -0500

    Remove refresh controller and fix bugs

--- src/containers/DeckInfoPage.js
+++ src/containers/DeckInfoPage.js
@@ -1,32 +1,63 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
-  Text,
+  ScrollView,
   View
 } from 'react-native';
+import firebase from 'react-native-firebase';
 import headerNavConfig from '../config/navigationOptions';
 import CreateCard from '../components/CreateCard';
+import CardListItem from '../components/CardListItem';
 
 class DeckInfoPage extends Component {
   static navigationOptions = ({ navigation }) => ({ ...headerNavConfig, title: navigation.state.params.deck.title });
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSwiping: false,
+      isRefreshing: false,
+      isModalVisible: false,
+      uid: firebase.auth().currentUser.uid
+    };
+    this.onSwipe = this.onSwipe.bind(this);
+  }
+
+  onSwipe(isSwiping) {
+    this.setState({ isSwiping });
+  }
 
   render() {
     return (
       <View>
         <CreateCard
-          uid={this.props.uid}
+          uid={this.state.uid}
           deckId={this.props.deck.id}
           dispatch={this.props.dispatch}
         />
-        {this.props.deck.cards.map(card => <Text>{card.english} {card.meaning}</Text>)}
+
+        <ScrollView
+          scrollEnabled={!this.state.isSwiping}
+          style={{ marginTop: 15 }}
+        >
+          {this.props.deck.cards && this.props.deck.cards.map((card) => (
+            <CardListItem
+              key={card.id}
+              card={card}
+              uid={this.props.uid}
+              onSwipe={this.onSwipe}
+              dispatch={this.props.dispatch}
+              navigate={this.props.navigation.navigate}
+            />
+          ))}
+      </ScrollView>
       </View>
     );
   }
 }
 
 const mapStateToProps = (state, props) => ({
-  deck: state.decks.find((deck) => deck.id === props.navigation.state.params.deck.id),
-  uid: state.user.uid
+  deck: state.decks.find((deck) => deck.id === props.navigation.state.params.deck.id)
 });
 
 export default connect(mapStateToProps)(DeckInfoPage);
--- src/containers/DecksPage.js
+++ src/containers/DecksPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
-  RefreshControl,
   ScrollView,
   View
 } from 'react-native';
@@ -9,7 +8,6 @@ import firebase from 'react-native-firebase';
 import headerNavConfig from '../config/navigationOptions';
 import DeckListItem from '../components/DeckListItem';
 import CreateDeck from '../components/CreateDeck';
-import { fetchDecks } from '../actions/decks';
 
 class DecksPage extends Component {
   static navigationOptions = { ...headerNavConfig, title: 'Decks' };
@@ -36,17 +34,10 @@ class DecksPage extends Component {
       uid: firebase.auth().currentUser.uid
     };
     this.onSwipe = this.onSwipe.bind(this);
-    this.onRefresh = this.onRefresh.bind(this);
   }
 
   onSwipe(isSwiping) {
     this.setState({ isSwiping });
-  }
-
-  onRefresh() {
-    this.setState({ isRefreshing: true });
-    fetchDecks(this.state.uid);
-    this.setState({ isRefreshing: false });
   }
 
   render() {
@@ -57,12 +48,6 @@ class DecksPage extends Component {
           dispatch={this.props.dispatch}
         />
         <ScrollView
-          refreshControl={
-            <RefreshControl
-              refreshing={this.state.isRefreshing}
-              onRefresh={this.onRefresh}
-            />
-          }
           scrollEnabled={!this.state.isSwiping}
         >
           {this.props.decks.length > 0 && this.props.decks.map((deck, i) => (
@@ -84,7 +69,6 @@ class DecksPage extends Component {
 
 const mapStateToProps = (state) => ({
   decks: state.decks,
-  user: state.user
 });
 
 export default connect(mapStateToProps)(DecksPage);
